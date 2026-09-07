46e4090b41e306293c3e4dbe9148e1ef94d22eaa
Денис Маняхин <salumbek.den@gmail.com>
2026-09-07 21:01:18 +0300

    Initialize floating overlay panel controls

--- bootstrap.js
+++ bootstrap.js
@@ -2,5 +2,6 @@
 // Mechanics extensions must finish registering before app.js restores a project.
 await import('./runtime-extensions.js')
 await import('./app.js')
+await import('./overlay-ui.js')
 await import('./testlab.js')
 await import('./powertrain-ui.js')
