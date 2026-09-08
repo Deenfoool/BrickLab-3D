f287845f0107b0bb93e5c30b2711fba886b89b7e
Денис Маняхин <salumbek.den@gmail.com>
2026-09-08 22:18:27 +0300

    parts: add RU/EN localization for PARTS-3

---
+++ parts3/i18n-parts-3.js
@@ -0,0 +1,36 @@
+import { PARTS } from '../parts.js'
+
+export const PARTS3_I18N_VERSION = 'parts-3-i18n-v1'
+
+const RU = Object.freeze({
+  'axle-2': ['Ось 2L', 'Короткая крестовая ось 2L с фасками на торцах'],
+  'axle-9': ['Ось 9L', 'Длинная крестовая ось 9L с фасками на торцах'],
+  bush: ['Втулка оси', 'Полноразмерный фиксатор и проставка для крестовой оси'],
+  'half-bush': ['Полувтулка', 'Компактный фиксатор и проставка половинной ширины'],
+  'steering-tie-rod-5': ['Рулевая тяга 5L', 'Жёсткая рулевая тяга 5L с вертикальными шарнирными проушинами'],
+  'wheel-hub': ['Ступица колеса', 'Свободно вращающаяся ступица с подшипником и крестовой осью под колесо'],
+  'wheel-narrow': ['Узкое колесо', 'Узкая дорожная шина с низким сопротивлением качению'],
+  'wheel-offroad-large': ['Большое внедорожное колесо', 'Крупная внедорожная шина с глубоким чередующимся протектором'],
+  'wheel-tractor': ['Тракторное колесо', 'Очень крупная сельскохозяйственная шина с агрессивными грунтозацепами'],
+})
+
+function isRussian() {
+  return document.documentElement.lang === 'ru' || localStorage.getItem('bricklab.ui.language.v1') === 'ru'
+}
+
+function applyParts3Language() {
+  const ru = isRussian()
+  for (const part of PARTS) {
+    const translated = RU[part.id]
+    if (!translated) continue
+    part.__parts3EnglishName ??= part.name
+    part.__parts3EnglishDescription ??= part.description
+    part.name = ru ? translated[0] : part.__parts3EnglishName
+    part.description = ru ? translated[1] : part.__parts3EnglishDescription
+  }
+  document.getElementById('partSearch')?.dispatchEvent(new Event('input', { bubbles: true }))
+  window.dispatchEvent(new CustomEvent('bricklab:partcatalogchange', { detail: { pack: PARTS3_I18N_VERSION, translated: ru } }))
+}
+
+window.addEventListener('bricklab:languagechange', () => requestAnimationFrame(applyParts3Language))
+applyParts3Language()
