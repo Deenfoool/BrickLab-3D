1250345b150cc6fa67e515b399ea3c8d3e076a9f
Денис Маняхин <salumbek.den@gmail.com>
2026-09-10 20:14:40 +0300

    Stamp Connector V4 hardened runtime build

--- physics-error-ui.js
+++ physics-error-ui.js
@@ -1,5 +1,5 @@
 const BUILD_ID = 'PARTS-6'
-const BUILD_TAG = 'parts-6-20260910-connector-v4-physics-v4'
+const BUILD_TAG = 'parts-6-20260910-connector-v4-physics-v6'
 window.__bricklabBuildId = BUILD_ID
 window.__bricklabBuildTag = BUILD_TAG
 
