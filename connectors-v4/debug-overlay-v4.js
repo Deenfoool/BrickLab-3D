2fe94502a5241cccd476f38050c3f5b120cd379a
Денис Маняхин <salumbek.den@gmail.com>
2026-09-17 14:57:30 +0300

    Connectors V4: load safe auto-link hotkey

--- connectors-v4/debug-overlay-v4.js
+++ connectors-v4/debug-overlay-v4.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
+import './auto-link-v4.js?v=connector-auto-link-20260917-v1'
 
-export const CONNECTOR_DEBUG_VERSION_V4='connector-debug-overlay-v4.2.1'
+export const CONNECTOR_DEBUG_VERSION_V4='connector-debug-overlay-v4.3.0'
 const ROOT_NAME='__bricklabConnectorV4Debug'
 const MAX_ENDPOINTS_PER_PART=384
 let enabled=false
